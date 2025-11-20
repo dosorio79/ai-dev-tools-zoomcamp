bbecde9269a3e18118ecfd65be832ac867164d4c
Daniel Osorio <daniel.s.osorio@gmail.com>
2025-11-20 00:15:58 +0000

    Add Todo app functionality with views and templates - CODEX version

--- 01-todo/todo/todoapp/views.py
+++ 01-todo/todo/todoapp/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from .models import Todo
+
+
+def todo_list(request):
+    # Create on POST, otherwise show the list
+    if request.method == "POST":
+        title = request.POST.get("title", "").strip()
+        if title:
+            Todo.objects.create(title=title)
+        return redirect("todo_list")
+
+    todos = Todo.objects.order_by("-created_at")
+    return render(request, "todoapp/list.html", {"todos": todos})
+
+
+def toggle_todo(request, pk):
+    if request.method != "POST":
+        return redirect("todo_list")
+
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.completed = not todo.completed
+    todo.save(update_fields=["completed"])
+    return redirect("todo_list")
+
+
+def delete_todo(request, pk):
+    if request.method != "POST":
+        return redirect("todo_list")
+
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.delete()
+    return redirect("todo_list")
